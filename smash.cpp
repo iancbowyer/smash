eec19dd6cd68c97b6b4b63f397a3ecf975e6023f
Ian Bowyer <iancbowyer@live.co.uk>
2017-09-01 03:19:15 +0100

    perform first collision

--- smash.cpp
+++ smash.cpp
@@ -18,14 +18,39 @@ int smash(int a, int b){
     b /= 10;
   }
 
-  for (int x = 0 ; x < a_vec.size(); ++x){
+  std::vector<int> a_surv;
+  std::vector<int> b_surv;
+
+  for (int x = 0; x < a_vec.size(); ++x){
     //std::cout << a_vec[x] << " " << b_vec[x] << std::endl;
-    if ((a_vec[x] * b_vec[x]) % 2 == 0){
-      
+    if (a_vec[x] == b_vec[x]){
+      //both survive
+      a_surv.push_back(a_vec[x]);
+      b_surv.push_back(b_vec[x]);
+    }
+    else if ((a_vec[x] * b_vec[x]) % 2 == 0){
+      //even
+      if (a_vec[x] > b_vec[x]){
+	a_surv.push_back(a_vec[x]);
+      }
+      else{
+	b_surv.push_back(b_vec[x]);
+      }
     }
     else{
-
+      //odd
+      if (a_vec[x] > b_vec[x]){
+	b_surv.push_back(b_vec[x]);
+      }
+      else{
+	a_surv.push_back(a_vec[x]);
+      }
+      
     }
+  }
+
+  for (int x = 0; x < b_surv.size(); ++x){
+    std::cout << b_surv[x] << std::endl;
   }
   
   return 1;
