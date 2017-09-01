0012bafaabf9aaf0852bdbbe6c69b454c1a477a0
Ian Bowyer <iancbowyer@live.co.uk>
2017-09-01 03:05:22 +0100

    ensure both vectors are the same size

--- smash.cpp
+++ smash.cpp
@@ -11,18 +11,21 @@ int smash(int a, int b){
   std::vector<int> a_vec;
   std::vector<int> b_vec;
 
-  while (a != 0){
+  while (a != 0 || b != 0){
     a_vec.push_back(a%10);
     a /= 10;
-  }
-
-  while(b != 0){
     b_vec.push_back(b%10);
     b /= 10;
   }
 
-  for (auto it = a_vec.begin(); it < a_vec.end(); ++it){
-    std::cout << *it << std::endl;
+  for (int x = 0 ; x < a_vec.size(); ++x){
+    //std::cout << a_vec[x] << " " << b_vec[x] << std::endl;
+    if ((a_vec[x] * b_vec[x]) % 2 == 0){
+      
+    }
+    else{
+
+    }
   }
   
   return 1;
